48b68b15dd3dcb8072f405de2dbe82c51a6d6f2e
Fakem1ner <103760918+Fakem1ner@users.noreply.github.com>
2023-01-07 15:54:49 +0400

    Add files via upload

---
+++ product/js/app.js
@@ -0,0 +1,54 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/modules/multypage-list.js
+let openInfo = document.querySelector('#multypage-open');
+
+openInfo.addEventListener('click', e => {
+    document.documentElement.classList.toggle('_multypage-opened');
+});
+;// CONCATENATED MODULE: ./src/js/scripts/script.js
+new Swiper('.swiper', {
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 32,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    }
+  },
+  autoplay: {
+    delay: 2500
+  },
+  centeredSlides: true
+});
+
+let starRatings = document.querySelectorAll('[data-stars]');
+
+starRatings.forEach(starRating => {
+  for(let i = 0; i < Number(starRating.getAttribute('data-stars')); i++) {
+    starRating.innerHTML = `${starRating.innerHTML} <svg class="review-reviews__star review-reviews__star_fill"><use href="img/icons/icons.svg#star"></use></svg>`
+  }
+  for(let i = 0; i < 5 - Number(starRating.getAttribute('data-stars')); i++) {
+    starRating.innerHTML = `${starRating.innerHTML} <svg class="review-reviews__star review-reviews__star_empty"><use href="img/icons/icons.svg#star"></use></svg>`
+  }
+});
+;// CONCATENATED MODULE: ./src/js/app.dev.js
+
+
+
+
+console.log('hello')
+/******/ })()
+;
