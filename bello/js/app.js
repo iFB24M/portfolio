e54cccee3179343c62fd94bea059d384bff9b131
Fakem1ner <103760918+Fakem1ner@users.noreply.github.com>
2023-02-05 21:25:27 +0400

    Add files via upload

---
+++ bello/js/app.js
@@ -0,0 +1,20 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/modules/multypage-list.js
+//import '../../scss/base/multypage.scss';
+
+let openInfo = document.querySelector('#multypage-open');
+
+openInfo.addEventListener('click', e => {
+    document.documentElement.classList.toggle('_multypage-opened');
+});
+;// CONCATENATED MODULE: ./src/js/app.dev.js
+
+
+
+
+
+/******/ })()
+;
